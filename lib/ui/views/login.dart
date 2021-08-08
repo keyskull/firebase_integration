1b32f0c17b0c333f44c08e3fdd9e724d06ddab34
Cullen Lee <a410615903@gmail.com>
2021-08-08 16:40:14 -0700

    Initial Commit

---
+++ lib/ui/views/login.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_universal_router/path_handler.dart';
+import 'package:localization/generated/l10n.dart';
+import 'package:provider/provider.dart';
+
+import '../components/buttons.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        signInButton(),
+        googleSignInButton(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+                onPressed: () =>
+                    Provider.of<PathHandler>(context, listen: false)
+                        .changePath('privacy-policy'),
+                child: Text(S.of(context).privacy_policy)),
+            TextButton(
+                onPressed: () =>
+                    Provider.of<PathHandler>(context, listen: false)
+                        .changePath('terms-of-service'),
+                child: Text(S.of(context).terms_of_service))
+          ],
+        )
+      ],
+    );
+  }
+}
