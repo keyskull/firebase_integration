29a9512130850dd6fe25fbcf8bef537a96c9e4f6
Cullen Lee <a410615903@gmail.com>
2021-08-08 20:41:06 -0700

    Updated bundle changes.

--- lib/ui/views/login.dart
+++ lib/ui/views/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_universal_router/path_handler.dart';
+import 'package:flutter_universal_router/route.dart';
 import 'package:localization/generated/l10n.dart';
 import 'package:provider/provider.dart';
 
