4d9d60bc3868b1abbd6712da1b4bda8819e02f68
Cullen Lee <a410615903@gmail.com>
2021-08-09 19:33:29 -0700

    Updated firebase.dart

--- lib/firebase.dart
+++ lib/firebase.dart
@@ -1,18 +1,41 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_universal_router/route.dart' as router;
 
 class FirebaseIntegration {
   static final firebaseAnalytics = FirebaseAnalytics();
   static final auth = FirebaseAuth.instance;
-  _init() async {
+  static final fireStore = FirebaseFirestore.instance;
+  initialize() async {
     WidgetsFlutterBinding.ensureInitialized();
+    router.addRoutePathListeners((routePath) {
+      FirebaseIntegration.firebaseAnalytics
+          .setCurrentScreen(
+              screenName: routePath.routeName,
+              screenClassOverride:
+                  routePath.getRouteInstance.getPage().runtimeType.toString())
+          .then((value) => log(
+              'firebaseAnalytics.setCurrentScreen(screenName: ${routePath.routeName})',
+              name: 'firebaseAnalytics.setCurrentScreen'))
+          .catchError(
+        (Object error) {
+          debugPrint(': $error');
+        },
+        test: (Object error) => error is PlatformException,
+      );
+      FirebaseIntegration.firebaseAnalytics.logEvent(name: 'page_view');
+    });
     await Firebase.initializeApp();
   }
 
   FirebaseIntegration() {
-    _init().then((_){
+    initialize().then((_) {
       firebaseAnalytics.logAppOpen();
       auth.userChanges().listen((User? user) {
         if (user == null) {
