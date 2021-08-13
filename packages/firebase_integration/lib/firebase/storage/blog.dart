314f1f889e331be563edd66f034ac80867948408
Cullen Lee <a410615903@gmail.com>
2021-08-13 01:03:23 -0700

    Updating dependent packages

--- packages/firebase_integration/lib/firebase/storage/blog.dart
+++
@@ -1,4 +0,0 @@
-import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
-
-final bolg = firebase_storage.FirebaseStorage.instanceFor(
-    bucket: 'secondary-storage-bucket');
--- packages/framework/lib/properties/navigation_content.dart
+++
@@ -1,7 +0,0 @@
-
-
-
-
-import 'package:flutter/material.dart';
-
-Map<String,bool Function(Notification notification)> globalNotificationListeners = {};
