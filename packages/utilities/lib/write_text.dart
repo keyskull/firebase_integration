fedfddd69a394d70d0ab3bbada6ee6264f264d01
Cullen Lee <a410615903@gmail.com>
2021-08-17 20:12:23 -0700

    Updated NOTICE text to write_text.dart

--- packages/utilities/lib/write_text.dart
+++ packages/utilities/lib/write_text.dart
@@ -1,14 +1,22 @@
+// Copyright 2004 老孟
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import 'package:flutter/material.dart';
 
 const int _defaultMillSeconds = 300;
 
-///
-/// desc: WriteText 是逐步显示文本的动画组件，像手写一样的效果。
-///
 class WriteText extends StatefulWidget {
-  ///
-  /// 数据
-  ///
   final String data;
 
   final bool showCursor;
