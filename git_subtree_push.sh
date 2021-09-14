7566c8e40b353eb547dec84296111afa6c114dd6
Cullen Lee <a410615903@gmail.com>
2021-09-13 18:32:36 -0700

    Updated git subtree process

--- git_subtree_push.sh
+++ git_subtree_push.sh
@@ -1,1 +1,1 @@
-git subtree push --prefix packages/firebase_integration https://github.com/keyskull/firebase_integration.git master
+git subtree push --prefix packages/firebase_integration https://github.com/keyskull/firebase_integration.git master --squash
