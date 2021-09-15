7f2fdd6df33269988bd606aa43949e69fb4c4f4f
Cullen Lee <a410615903@gmail.com>
2021-09-14 21:32:51 -0700

    Updated the usage for the Universal Router package;

--- lib/ui/views/login.dart
+++ lib/ui/views/login.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:localization/generated/l10n.dart';
-import 'package:provider/provider.dart';
 import 'package:universal_router/route.dart';
 
 import '../components/buttons.dart';
@@ -18,14 +17,10 @@ class Login extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextButton(
-                onPressed: () =>
-                    Provider.of<PathHandler>(context, listen: false)
-                        .changePath('privacy-policy'),
+                onPressed: () => UniversalRouter.changePath('privacy-policy'),
                 child: Text(S.of(context).privacy_policy)),
             TextButton(
-                onPressed: () =>
-                    Provider.of<PathHandler>(context, listen: false)
-                        .changePath('terms-of-service'),
+                onPressed: () => UniversalRouter.changePath('terms-of-service'),
                 child: Text(S.of(context).terms_of_service))
           ],
         )
