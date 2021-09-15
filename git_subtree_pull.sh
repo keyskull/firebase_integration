651ce8ab54b917489456d379b034b633fb21873e
Cullen Lee <a410615903@gmail.com>
2021-09-14 22:32:19 -0700

    Updated workflows

--- git_subtree_pull.sh
+++ git_subtree_pull.sh
@@ -1,1 +1,1 @@
-git subtree pull --prefix packages/firebase_integration https://github.com/keyskull/firebase_integration.git master
+git subtree pull --prefix packages/firebase_integration https://github.com/keyskull/firebase_integration.git master --squash
