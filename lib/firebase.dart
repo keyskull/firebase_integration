e6361f56bf523b76422c0b60abee8e6178ff6ced
Cullen Lee <a410615903@gmail.com>
2021-10-21 15:06:31 -0700

    Fixed path listener execute problem;

--- lib/firebase.dart
+++ lib/firebase.dart
@@ -1,20 +1,25 @@
 import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cullen_utilities/custom_log_printer.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:logger/logger.dart';
 import 'package:universal_router/route.dart';
 
 class FirebaseIntegration {
+  final logger = Logger(printer: CustomLogPrinter('FirebaseIntegration'));
+
   static final firebaseAnalytics = FirebaseAnalytics();
   static final auth = FirebaseAuth.instance;
   static final fireStore = FirebaseFirestore.instance;
   initialize() async {
     WidgetsFlutterBinding.ensureInitialized();
     UniversalRouter.addRoutePathListeners((routePath) {
+      logger.i('RoutePathListeners run.');
       FirebaseIntegration.firebaseAnalytics
           .setCurrentScreen(
               screenName: routePath.routeName,
